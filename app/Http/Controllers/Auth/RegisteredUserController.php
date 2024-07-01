4c8bba9778bd1bca36ac57bbc8909779db58497a
Debora7 <burnichinaomi@gmail.com>
2024-07-01 18:39:38 +0300

    Fixed register and login functionality.

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,15 +32,22 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
+            'email' => 'required|string|lowercase|email|max:255|unique:' . User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'cui' => 'nullable|string|max:255',
         ]);
 
-        $user = User::create([
+        $userData = [
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-        ]);
+        ];
+
+        if ($request->filled('cui')) {
+            $userData['cui'] = $request->cui;
+        }
+
+        $user = User::create($userData);
 
         event(new Registered($user));
 
