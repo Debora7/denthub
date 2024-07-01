052b5ee5b1f44d582f2b2881be9a6a7e287990b0
Debora7 <burnichinaomi@gmail.com>
2024-07-01 22:51:21 +0300

    Roles on register.

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Auth/Login');
-});
+})->name('login');
 
 Route::get('/register', function () {
     return Inertia::render('Auth/Register');
