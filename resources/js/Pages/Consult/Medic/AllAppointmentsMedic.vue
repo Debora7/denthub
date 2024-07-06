772a11042895b8b6185a68c3de1b49d02095e63e
Debora7 <burnichinaomi@gmail.com>
2024-07-07 01:29:27 +0300

    User phone nuber.

--- resources/js/Pages/Consult/Medic/AllAppointmentsMedic.vue
+++ resources/js/Pages/Consult/Medic/AllAppointmentsMedic.vue
@@ -149,6 +149,12 @@ const openNewAppointmentModal = () => {
                                             scope="col"
                                             class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                                         >
+                                            Telefon
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                        >
                                             Ora programării
                                         </th>
                                         <th
@@ -182,6 +188,11 @@ const openNewAppointmentModal = () => {
                                             class="px-6 py-4 whitespace-nowrap text-sm text-gray-900"
                                         >
                                             {{ appointment.user.name }}
+                                        </td>
+                                        <td
+                                            class="px-6 py-4 whitespace-nowrap text-sm text-gray-900"
+                                        >
+                                            {{ appointment.user.phone ?? "-" }}
                                         </td>
                                         <td
                                             class="px-6 py-4 whitespace-nowrap text-sm text-gray-900"
