155fce0536e87ead335fef17b87b1782060d9249
Debora7 <burnichinaomi@gmail.com>
2024-07-07 17:16:48 +0300

    Added seeders for all the cities in counties.

--- database/seeders/CountiesTableSeeder.php
+++ database/seeders/CountiesTableSeeder.php
@@ -5,58 +5,34 @@ namespace Database\Seeders;
 use App\Models\County;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use League\Csv\Reader;
 
 class CountiesTableSeeder extends Seeder
 {
     public function run()
     {
-        $counties = [
-            ['name' => 'Alba'],
-            ['name' => 'Arad'],
-            ['name' => 'Argeș'],
-            ['name' => 'Bacău'],
-            ['name' => 'Bihor'],
-            ['name' => 'Bistrița-Năsăud'],
-            ['name' => 'Botoșani'],
-            ['name' => 'Brașov'],
-            ['name' => 'Brăila'],
-            ['name' => 'Buzău'],
-            ['name' => 'Caraș-Severin'],
-            ['name' => 'Călărași'],
-            ['name' => 'Cluj'],
-            ['name' => 'Constanța'],
-            ['name' => 'Covasna'],
-            ['name' => 'Dâmbovița'],
-            ['name' => 'Dolj'],
-            ['name' => 'Galați'],
-            ['name' => 'Giurgiu'],
-            ['name' => 'Gorj'],
-            ['name' => 'Harghita'],
-            ['name' => 'Hunedoara'],
-            ['name' => 'Ialomița'],
-            ['name' => 'Iași'],
-            ['name' => 'Ilfov'],
-            ['name' => 'Maramureș'],
-            ['name' => 'Mehedinți'],
-            ['name' => 'Mureș'],
-            ['name' => 'Neamț'],
-            ['name' => 'Olt'],
-            ['name' => 'Prahova'],
-            ['name' => 'Satu Mare'],
-            ['name' => 'Sălaj'],
-            ['name' => 'Sibiu'],
-            ['name' => 'Suceava'],
-            ['name' => 'Teleorman'],
-            ['name' => 'Timiș'],
-            ['name' => 'Tulcea'],
-            ['name' => 'Vaslui'],
-            ['name' => 'Vâlcea'],
-            ['name' => 'Vrancea'],
-            ['name' => 'București'], // Bucharest is the capital and also considered a county
-        ];
+        // Load the CSV file
+        $csvPath = base_path('/database/seeders/cities.csv');
 
-        foreach ($counties as $county) {
-            County::create($county);
+        // Load the CSV document from a file path
+        $csv = Reader::createFromPath($csvPath, 'r');
+        $csv->setHeaderOffset(0);
+        $records = $csv->getRecords();
+
+        // Initialize an array to store unique counties
+        $uniqueCounties = [];
+
+        // Loop through the records and add unique counties to the array
+        foreach ($records as $record) {
+            $countyName = $record['county_name'];
+            if (!in_array($countyName, $uniqueCounties)) {
+                $uniqueCounties[] = $countyName;
+            }
+        }
+
+        // Loop through the unique counties array and create records in the database
+        foreach ($uniqueCounties as $county) {
+            County::create(['name' => $county]);
         }
     }
 }
